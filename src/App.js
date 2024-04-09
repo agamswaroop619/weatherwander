b005f6844faab53c5f834e329e3c24ae6ae813c7
agamswaroop619 <agam.swaroop619@gmail.com>
2024-04-09 11:07:58 +0530

    add: final changes for styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import "./App.css";
 function App() {
   const [location, setLocation] = useState({ latitude: null, longitude: null });
   const [weather, setWeather] = useState(null);
